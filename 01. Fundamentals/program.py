26de39917e7f9b2317045bddec73370c63086640
Pauline Korukundo <pktpaulie@gmail.com>
2023-07-05 17:14:09 +0300

    Add User Singleton Class

--- 01. Fundamentals/program.py
+++ 01. Fundamentals/program.py
@@ -2,11 +2,47 @@ from abc import ABC, abstractmethod
 
 
 class User:
-    def __init__(self, username, password):
+    """class to demonstrate singleton pattern"""
+    __instance = None
+
+    @staticmethod
+    def login(username, password):
+        """get the current instance of the class"""
+        if User.__instance is None:
+            return User(username, password)
+        return User.__instance
+
+    def __init__(self, username: str, password: str):
+        """ initialise/ set the instance itself"""
+        if User.__instance is None:
+            User.__instance = self
+            self.username = username
+            self.password = password
+        else:
+            raise Exception("User logged in") #prevent creation of multiple objects
+        
+    @staticmethod
+    def logout():
+        """log out/ end session for current instance"""
+        User.__instance = None
+
+    def set_username(self, username: str):
+        """set username"""
         self.username = username
+
+    def set_password(self, password: str):
+        """set the password"""
         self.password = password
 
-    #setters
+    def get_username(self):
+        """return the username"""
+        return User.__instance.username # updated the getter for username
+    
+    def get_password(self):
+        """return the username"""
+        return User.__instance.password # updated the getter for username
+
+    """ #setters
     def set_username(self, username):
         self.username = username
 
@@ -18,7 +54,7 @@ class User:
         return self.username
     
     def get_password(self):
-        return self.password
+        return self.password """
 
 
 class InputDevice(ABC):
@@ -92,12 +128,12 @@ class Memory(ABC):
 
 class SSD(Memory):
     def store(self, data):
-        print("--------storing data on SSD----------\n")
+        print("------------Storing data on SSD------------------\n")
 
 
 class InternalMemory(Memory):
     def store(self, data):
-        print("---- Storage Process ------------------")
+        print("-------------- Storage Process ------------------")
         print("Step1: Receive data to be stored")
         print("Step2: Open Internal memory where data is to stored")
         print("Step3: Prepare for data storage operation")
@@ -114,7 +150,7 @@ class OutputDevice(ABC):
 
 class Monitor(OutputDevice):
     def output(self, data):
-        print("---- Outputing to Monitor ------------------")
+        print("----------- Outputing to Monitor ------------------")
         print("Step1: Receive data to be output")
         print("Step2: Open monitor where data is to displayed")
         print("Step3: Prepare for data output operation")
@@ -125,7 +161,7 @@ class Monitor(OutputDevice):
 
 class Projector(OutputDevice):
     def output(self, data):
-        print("---- Outputing to Projector ------------------")
+        print("---------- Outputing to Projector ------------------")
         print("Step1: Receive data to be output")
         print("Step2: Open projector where data is to displayed")
         print("Step3: Prepare for data output operation")
@@ -136,11 +172,11 @@ class Projector(OutputDevice):
 
 
 class Computer(ABC):
-    def __init__(self, input_device, processor_chip, memory, output_device):
+    def __init__(self, input_device, processor_chip, memory, output_device, user:User):
         # Fields
         self.brand = ""
         self.model = ""
-        self.user = User(username=None, password=None)
+        self.user = user
         self.input_device = input_device
         self.processor_chip = processor_chip
         self.memory = memory
@@ -173,7 +209,6 @@ class Computer(ABC):
         return self.output_device
 
 
-
     # Methods
     def input(self, data):
         self.input_device.input(data)
@@ -187,6 +222,15 @@ class Computer(ABC):
     def output(self, data):
         self.output_device.output(data)
 
+    # User Methods        
+    def set_user(self, user):
+        """ set the user"""
+        self.user = user
+    
+    def get_user(self):
+        """ return the user"""
+        return self.user
+        #return self.user.get_password , self.user.get_password
 
 # Inheritance: Desktop is inheriting from Computer
 class Desktop(Computer):
@@ -213,17 +257,9 @@ class Walltop(Computer):
     pass
 
 
-
-user = User("Shafic", "profic")
-
-print(user.get_username(), user.get_password())
-
-user.set_username("Bruce")
-user.set_password("admin")
-
-print(user.get_username(), user.get_password())
-
-computer = Desktop(Keyboard(), Nvidia(), InternalMemory(), Projector())
+computer = Desktop(Keyboard(), Nvidia(), InternalMemory(), Projector(),User.login("Franco", "123"))
+print(computer.user.get_username(), computer.user.get_password())
+#print(computer.get_user())
 computer.set_input(TouchScreen())
 computer.input("blah")
 computer.set_memory(SSD())
@@ -231,18 +267,25 @@ computer.process("see")
 computer.store("1")
 computer.output("12")
 
+computer.user.logout() # logout first user
 
-computer = Laptop(Mouse(), AMD(), SSD(), Projector())
+computer = Laptop(Mouse(), AMD(), SSD(), Projector(),User.login("Pauline", "pkt"))
+print(computer.user.get_username(), computer.user.get_password())
+#print(computer.get_user())
 computer.input("soo")
 computer.process("ts")
 computer.store("2")
 computer.set_output_device(Monitor())
 computer.output("12")
 
-computer = Walltop(Keyboard(), Intel(), SSD(), Monitor())
+#computer.user.logout() # logout second user
+
+computer = Walltop(Keyboard(), Intel(), SSD(), Monitor(), User.login("Oliver", "pasd"))
+print(computer.user.get_username(), computer.user.get_password())
+#print(computer.get_user())
 computer.set_processor_chip(Nvidia())
 computer.process("s")
-#computer.set_memory(InternalMemory())
+computer.set_memory(InternalMemory())
 computer.store("3")
 computer.set_input(TouchScreen())
 computer.input("simsim")
